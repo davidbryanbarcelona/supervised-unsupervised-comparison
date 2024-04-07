14dd7bfafa931f29c969cdb731e28abea3e0d471
David Bryan Barcelona <99592635+davidbryanbarcelona@users.noreply.github.com>
2024-04-07 17:41:46 +0800

    Update Clustering_Task.py

--- Clustering_Task.py
+++ Clustering_Task.py
@@ -13,7 +13,7 @@ def app():
     West Visayas State University"""
     st.text(text)
 
-    st.image('mushroom.jpg', caption="The Iris Dataset""")
+    st.image('mushrooms.jpg', caption="The Iris Dataset""")
 
     text = """Data App: Supervised vs Unsupervised Learning Performance
     \nThis data app allows users to compare the performance of supervised learning (KNN) and unsupervised 
